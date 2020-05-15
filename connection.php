e4c27cc9f6b564e93957a3c25db0a8ecf1e1347c
Wenseslaus Raphael <wenseslaus96@gmail.com>
2020-05-15 08:28:09 +0300

    Web Dev Project - Hilton Hotel

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-$con=mysqli_connect("localhost","root","","hotel") or die('DATABASE connection failed');
+$con=mysqli_connect("localhost","brysonki_root","password123!@#","brysonki_hotel") or die('DATABASE connection failed');
 
 ?>
